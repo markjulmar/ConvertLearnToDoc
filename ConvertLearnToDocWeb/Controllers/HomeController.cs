5af0fd45e75ee4e5dad26f6913268448a07cd9d0
Mark Smith <mark@julmar.com>
2021-12-01 07:26:25 -0600

    Add logger to converter.

--- ConvertLearnToDocWeb/Controllers/HomeController.cs
+++ ConvertLearnToDocWeb/Controllers/HomeController.cs
@@ -64,7 +64,8 @@ namespace ConvertLearnToDocWeb.Controllers
             try
             {
                 _logger.LogDebug($"LearnToDocX(repo:{repo}, branch:{branch}, folder:{folder}: outputFile={outputFile})");
-                await LearnToDocx.ConvertAsync(repo, branch, folder, outputFile, _configuration.GetValue<string>("GitHub:Token"));
+                await LearnToDocx.ConvertAsync(repo, branch, folder, outputFile, 
+                    _configuration.GetValue<string>("GitHub:Token"), s => _logger.LogDebug(s));
             }
             catch (Exception ex)
             {
@@ -113,7 +114,7 @@ namespace ConvertLearnToDocWeb.Controllers
             try
             {
                 _logger.LogDebug($"DocxToLearn(inputFile:{tempFile}, outputPath:{outputPath})");
-                await DocxToLearn.ConvertAsync(tempFile, outputPath);
+                await DocxToLearn.ConvertAsync(tempFile, outputPath, s => _logger.LogDebug(s));
             }
             catch (Exception ex)
             {
