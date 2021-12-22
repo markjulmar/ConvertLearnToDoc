c578016c722cb25af109a8caa36f21ffc6d067ba
Mark Smith <mark@julmar.com>
2021-12-22 15:59:10 -0600

    Add version number to header.

--- ConvertLearnToDocWeb/Program.cs
+++ ConvertLearnToDocWeb/Program.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.AspNetCore.Hosting;
+﻿using System;
+using System.Globalization;
+using System.Reflection;
+using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Hosting;
 
 namespace ConvertLearnToDocWeb
@@ -13,5 +16,26 @@ namespace ConvertLearnToDocWeb
                     webBuilder.UseStartup<Startup>();
                 }).Build().Run();
         }
-    }
+
+		public static DateTime GetLinkerTime(Assembly assembly = null)
+		{
+			if (assembly == null)
+				assembly = Assembly.GetExecutingAssembly();
+
+			const string BuildVersionMetadataPrefix = "+build";
+
+			var attribute = assembly.GetCustomAttribute<AssemblyInformationalVersionAttribute>();
+			if (attribute?.InformationalVersion != null)
+			{
+				var value = attribute.InformationalVersion;
+				var index = value.IndexOf(BuildVersionMetadataPrefix);
+				if (index > 0)
+				{
+					value = value[(index + BuildVersionMetadataPrefix.Length)..];
+					return DateTime.ParseExact(value, "yyyy-MM-ddTHH:mm:ss:fffZ", CultureInfo.InvariantCulture);
+				}
+			}
+			return default;
+		}
+	}
 }
