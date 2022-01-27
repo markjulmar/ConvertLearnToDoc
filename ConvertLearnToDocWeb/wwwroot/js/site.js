8ce058c3aec03fd1b7408f8f37747fde4d9aac90
Mark Smith <mark@julmar.com>
2022-01-27 09:19:18 -0600

    Update packages, add npm to MSBuild script

--- ConvertLearnToDocWeb/wwwroot/js/site.js
+++ ConvertLearnToDocWeb/wwwroot/js/site.js
@@ -2,3 +2,6 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+$(document).on('change', '.custom-file-input', function (event) {
+    $(this).next('.custom-file-label').html(event.target.files[0].name);
+})
